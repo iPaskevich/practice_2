1ab1605e1cbf2b6df833908ace396087872dcbee
Илья Паскевич <paskevich@internet.ru>
2024-03-04 23:16:13 +0300

    Задача №2_Типы данных

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double a = 33.3;
+        byte b = 22;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
